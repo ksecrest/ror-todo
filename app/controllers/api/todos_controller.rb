f19a7300ec8db4409de62e3a2275be86759ffd63
Katie Secrest <katie.secrest@gmail.com>
2021-06-15 19:24:08 -0600

    end of controller

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,15 +1,35 @@
 class Api::TodosController < ApplicationController
-    # needs to return html, xml or json
-
-    def index
-        render json: Todo.all
+    def index 
+      render json: Todo.all
     end
-
-    def create
-
+    def show 
+      render json: Todo.find(params[:id])
     end
-
-    def 
-
+    def create 
+      todo = Todo.new(todo_params)
+      if todo.save 
+        render json: todo
+      else
+        render json: { errors: todo.errors }, status: :unprocessable_entity
+      end
     end
-end
+    def update 
+      todo = Todo.find(params[:id])
+      if todo.update(todo_params)
+        render json: todo
+      else
+        render json: { errors: todo.errors }, status: :unprocessable_entity
+      end
+    end
+    def destroy
+      Todo.find(params[:id]).destroy
+      render json: { message: 'todo deleted' }
+    end
+    private 
+      # { todo: { title: "adsf", complete: false }}
+      def todo_params
+        params.require(:todo).permit(:title, :complete)
+      end
+  end
+
+# 422 error is a controller error
